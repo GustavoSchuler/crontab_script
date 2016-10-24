977ce700782a6c24f4d2567f05b4cd68464d80a2
gustavo <gustavo@git@fcc.com.br>
2016-10-24 17:32:00 -0200

    changing data

--- crontab_script.sh
+++ crontab_script.sh
@@ -18,28 +18,30 @@ output=$(curl --silent -H 'Accept: application/vnd.twitchtv.v3+json' -X GET http
 max=$( echo "$output" | jq length )
 
 # Create containers
-for i in seq 1 $max
+for i in `seq 1 $max`
 do
+	
+	z=$(echo $(( i - 1 )))
 
-	_payd=$( echo "$output" | jq '.[$i]["pago"]' )
+	_payd=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["pago"]' )
 
-	if [ "$_payd" == "1" ]; then
+	if [ "$_payd" == 1 ]; then
 
-		_domain=$( echo "$output" | jq '.[$i]["host"]' )
-		_hostname=$( echo "$output" | jq '.[$i]["uuid"]' )
-		_username=$( echo "$output" | jq '.[$i]["email"]' )
-		_user_pass=$( echo "$output" | jq '.[$i]["senhasenha"]' )
-		_root_pass=$( echo "$output" | jq '.[$i]["senharoot"]' )
-		_mysql_root_pass=$( echo "$output" | jq '.[$i]["senhamysql"]' )
+		_domain=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["host"]' )
+		_hostname=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["uuid"]' )
+		_username=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["email"]' )
+		_user_pass=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["senhasenha"]' )
+		_root_pass=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["senharoot"]' )
+		_mysql_root_pass=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["senhamysql"]' )
 
 		#./new_container.sh -d $_domain -h $_hostname -u $_username -up $_user_pass -rp $_root_pass -mrp $_mysql_root_pass
 		echo 'DOMAIN BEING CREATED:'
-		echo 'DOMAIN: ' + $_domain
-		echo 'HOSTNAME: ' + $_hostname
-		echo 'USERNAME: ' + $_username
-		echo 'USER_PASS: ' + $_user_pass
-		echo 'ROOT_PASS:' + $_root_pass
-		echo 'MYSQL_ROOT_PASS: ' + $_mysql_root_pass 
+		echo 'DOMAIN: ' $_domain
+		echo 'HOSTNAME: ' $_hostname
+		echo 'USERNAME: ' $_username
+		echo 'USER_PASS: ' $_user_pass
+		echo 'ROOT_PASS:' $_root_pass
+		echo 'MYSQL_ROOT_PASS: ' $_mysql_root_pass 
 	fi
 
 done
