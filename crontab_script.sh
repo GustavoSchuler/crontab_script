993fea5a71ce165827ee5b714129bed9bba3da54
gustavo <gustavo@git@fcc.com.br>
2016-10-25 07:26:43 -0200

    Removing quotes with jq -r argument

--- crontab_script.sh
+++ crontab_script.sh
@@ -23,27 +23,27 @@ do
 	
 	z=$(echo $(( i - 1 )))
 
-	_payd=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["pago"]' )
+	_payd=$( echo "$output" | jq -r --arg z $z '.[$z | tonumber]["pago"]' )
 	
-	if [[ "$_payd" == '"1"' ]]
+	if [[ "$_payd" == "1" ]]
 	then
 
-		_domain=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["host"]' )
-		_hostname=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["uuid"]' )
-		_username=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["email"]' )
-		_user_pass=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["senhasenha"]' )
-		_root_pass=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["senharoot"]' )
-		_mysql_root_pass=$( echo "$output" | jq --arg z $z '.[$z | tonumber]["senhamysql"]' )
+		_domain=$( echo "$output" | jq -r --arg z $z '.[$z | tonumber]["host"]' )
+		_hostname=$( echo "$output" | jq -r --arg z $z '.[$z | tonumber]["uuid"]' )
+		_username=$( echo "$output" | jq -r --arg z $z '.[$z | tonumber]["email"]' )
+		_user_pass=$( echo "$output" | jq -r --arg z $z '.[$z | tonumber]["senhasenha"]' )
+		_root_pass=$( echo "$output" | jq -r --arg z $z '.[$z | tonumber]["senharoot"]' )
+		_mysql_root_pass=$( echo "$output" | jq -r --arg z $z '.[$z | tonumber]["senhamysql"]' )
 
 		./new_container.sh -d $_domain -h $_hostname -u $_username -up $_user_pass -rp $_root_pass -mrp $_mysql_root_pass
 		
 		echo 'DOMAIN BEING CREATED:'
-		echo 'DOMAIN: ' $_domain
-		echo 'HOSTNAME: ' $_hostname
-		echo 'USERNAME: ' $_username
-		echo 'USER_PASS: ' $_user_pass
+		echo 'DOMAIN:' $_domain
+		echo 'HOSTNAME:' $_hostname
+		echo 'USERNAME:' $_username
+		echo 'USER_PASS:' $_user_pass
 		echo 'ROOT_PASS:' $_root_pass
-		echo 'MYSQL_ROOT_PASS: ' $_mysql_root_pass 
+		echo 'MYSQL_ROOT_PASS:' $_mysql_root_pass 
 		
 	fi
 
